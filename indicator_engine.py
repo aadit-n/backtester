9f9bf8938bb297062511f1b2b9019d8336963785
Aadit Nair <81066624+aadit-n@users.noreply.github.com>
2025-07-18 12:36:49 +0400

    Update indicator_engine.py

--- indicator_engine.py
+++ indicator_engine.py
@@ -2,6 +2,83 @@ import tulipy as tp
 import pandas as pd
 import numpy as np
 
+
+OUTPUT_MAPPINGS = {
+    "di": ["plus_di", "minus_di"],
+    "dm": ["plus_dm", "minus_dm"],
+    "bbands": ["lower_band", "middle_band", "upper_band"],
+    "macd": ["macd", "signal", "histogram"],
+    "adosc": ["adosc"],
+    "stoch": ["%K", "%D"],
+    "stochrsi": ["stochrsi"],
+    "sar": ["sar"],
+    "ultosc": ["ultosc"],
+    "kvo": ["kvo"],
+    "vosc": ["vosc"],
+    "apo": ["apo"],
+    "aroon": ["aroon_up", "aroon_down"],
+    "aroonosc": ["aroonosc"],
+    "adx": ["adx"],
+    "adxr": ["adxr"],
+    "ad": ["ad"],
+    "ao": ["ao"],
+    "atr": ["atr"],
+    "bop": ["bop"],
+    "cci": ["cci"],
+    "cmo": ["cmo"],
+    "cvi": ["cvi"],
+    "dx": ["dx"],
+    "dpo": ["dpo"],
+    "emv": ["emv"],
+    "fisher": ["fisher", "fisher_signal"],
+    "fosc": ["fosc"],
+    "linreg": ["linreg"],
+    "linregintercept": ["linregintercept"],
+    "linregslope": ["linregslope"],
+    "marketfi": ["marketfi"],
+    "mass": ["mass"],
+    "mfi": ["mfi"],
+    "minidmi": ["minidmi"],
+    "minus_di": ["minus_di"],
+    "minus_dm": ["minus_dm"],
+    "mom": ["momentum"],
+    "msw": ["msw_sine", "msw_lead"],
+    "natr": ["natr"],
+    "nvi": ["nvi"],
+    "obv": ["obv"],
+    "plus_di": ["plus_di"],
+    "plus_dm": ["plus_dm"],
+    "ppo": ["ppo"],
+    "pvi": ["pvi"],
+    "roc": ["roc"],
+    "rocp": ["rocp"],
+    "rocr": ["rocr"],
+    "rocr100": ["rocr100"],
+    "rsi": ["rsi"],
+    "sma": ["sma"],
+    "tema": ["tema"],
+    "trange": ["trange"],
+    "trima": ["trima"],
+    "trix": ["trix"],
+    "tsf": ["tsf"],
+    "typprice": ["typprice"],
+    "var": ["var"],
+    "vhf": ["vhf"],
+    "vidya": ["vidya"],
+    "vwma": ["vwma"],
+    "wad": ["wad"],
+    "wilders": ["wilders"],
+    "willr": ["willr"],
+    "wma": ["wma"],
+    "zlema": ["zlema"],
+    "avgprice": ["avgprice"],
+    "medprice": ["medprice"],
+    "wcprice": ["wcprice"],
+    "wclprice": ["wclprice"],
+    "sum": ["sum"],
+    "sub": ["sub"],
+}
+
 def get_indicators(df, config):
     name = config["name"].lower()
     params = config["params"]
@@ -15,13 +92,17 @@ def get_indicators(df, config):
     result = func(*inputs, **params)
 
     if isinstance(result, tuple):
-        columns = [f"{name.upper()}_{i}_{'_'.join(map(str, params.values()))}" for i in range(len(result))]
+        friendly_names = OUTPUT_MAPPINGS.get(name, [f"{name}_{i}" for i in range(len(result))])
+        columns = [
+            build_col_name(name, params, suffix=friendly_names[i] if i < len(friendly_names) else str(i))
+            for i in range(len(result))
+        ]
         series_dict = {
             col: pd.Series(res, index=df.index[-len(res):])
             for col, res in zip(columns, result)
         }
     else:
-        col_name = f"{name.upper()}_{'_'.join(map(str, params.values()))}"
+        col_name = build_col_name(name, params)
         series_dict = {
             col_name: pd.Series(result, index=df.index[-len(result):])
         }
